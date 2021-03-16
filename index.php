bac81e916e7d059970046c8189226f1df01595bd
Jaspreet Singh <34741406+jas9reet@users.noreply.github.com>
2021-03-16 10:37:46 +0530

    Push

---
+++ index.php
@@ -0,0 +1,58 @@
+<!-- Code By Jas9reet -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Hackers Web </title>
+    <link rel="icon" href="img/alian.png"  type="image/png">
+    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<style type="text/css">
+  
+      a {
+        color: white;
+      }
+      a:visited {
+        color: white;
+      }
+      a:hover, a:active, a:focus {
+        color:white;
+      }
+
+</style>
+<body class="body">
+
+    <canvas id="matrixRain"></canvas>
+
+    <h1 style="color: white; text-align: center; "><br><br></h1>
+
+<center>
+      <div class="div">
+      <script src="js/main.js"></script>
+      <div id="main">
+        <div class="container-fluid">
+            <div class="container">
+              <nav class="navbar" class="a">
+                  <a href=""><b>Home</b></a>
+                  <a href=""><b>Team</b></a>
+                  <img width="40px" height="40px" src="img/alian.png">
+                  <a href=""><b>About</b></a>
+                  <a href=""><b>Contact</b></a>
+              </nav>
+            </div>
+        </div>
+       <img src="img/poster.png">
+      </div>
+        <br>
+      </div>
+</center>
+
+    <script type="text/javascript" src="js/matrix.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</body>
+</html> 
